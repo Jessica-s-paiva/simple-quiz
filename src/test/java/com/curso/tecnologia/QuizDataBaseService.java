288f62bba25880af81098993ceb7ec8b29bcc5a0
jessica.paiva <jessicapaivamvp@gmail.com>
2024-02-13 12:09:06 -0300

    Iniciando o quiz

---
+++ src/test/java/com/curso/tecnologia/QuizDataBaseService.java
@@ -0,0 +1,39 @@
+package com.curso.tecnologia;
+
+import com.curso.tecnologia.model.Question;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
+public class QuizDataBaseService {
+
+    public ArrayList<Question> showAllQuestions(){
+        ArrayList<Question> questionsList = new ArrayList<>();
+
+        questionsList.add(new Question( "Movie",  "Qual é o filme mais premiado da história do Oscar?",  "O Senhor dos Anéis: O Retorno do Rei"));
+        questionsList.add(new Question("Jogos", "Qual é o jogo mais vendido de todos os tempos?", "Minecraft"));
+        questionsList.add(new Question("Ciência", "Quem foi o inventor da teoria da relatividade?", "Albert Einstein"));
+        questionsList.add(new Question("Geografia", "Qual é a capital da França?", "Paris"));
+        questionsList.add(new Question("História", "Em que ano ocorreu a Revolução Francesa?", "1789"));
+        questionsList.add(new Question("Tecnologia", "Quem é o CEO da Tesla?", "Elon Musk"));
+        questionsList.add(new Question("Literatura", "Quem escreveu 'Dom Quixote'?", "Miguel de Cervantes"));
+        questionsList.add(new Question("Música", "Qual é a banda de rock mais vendida da história?", "The Beatles"));
+        questionsList.add(new Question("Esportes", "Qual é o esporte mais popular do mundo?", "Futebol"));
+        questionsList.add(new Question("Arte", "Quem pintou a Mona Lisa?", "Leonardo da Vinci"));
+
+        return questionsList;
+    }
+
+    public ArrayList<Question> findByTheme(ArrayList<Question> allQuestions, String theme) {
+        ArrayList<Question> filteredQuestionList = new ArrayList<>();
+
+        for (Question question : allQuestions){
+            if (question.getTheme().equalsIgnoreCase(theme)){ //o IgnoreCase é pra ignorar se é maiúscula ou minúscula
+                filteredQuestionList.add(question);
+            }
+        }
+        return filteredQuestionList;
+    }
+
+}
--- src/test/java/com/curso/tecnologia/SimpleQuizApplicationTests.java
+++ src/test/java/com/curso/tecnologia/SimpleQuizApplicationTests.java
@@ -1,13 +1,19 @@
 package com.curso.tecnologia;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SimpleQuizApplicationTests {
 
+	@Autowired
+	private QuizDataBaseService quizDataBaseService;
 	@Test
 	void contextLoads() {
+
+		System.out.println(quizDataBaseService.showAllQuestions());
+		System.out.println("pause");
 	}
 
 }
